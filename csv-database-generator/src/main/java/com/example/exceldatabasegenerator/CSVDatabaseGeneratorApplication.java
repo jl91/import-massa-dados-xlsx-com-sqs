2d42fa990863e9f0e2d44bd69f2ff3c3f1af1dde
John lennon de melo fernandes <jlfernandes91@hotmail.com>
2022-08-02 23:43:06 -0300

    adding csv database generator

---
+++ csv-database-generator/src/main/java/com/example/exceldatabasegenerator/CSVDatabaseGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.exceldatabasegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CSVDatabaseGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CSVDatabaseGeneratorApplication.class, args);
+    }
+
+}
---
+++ csv-database-generator/src/test/java/com/example/exceldatabasegenerator/CSVDatabaseGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exceldatabasegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CSVDatabaseGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
