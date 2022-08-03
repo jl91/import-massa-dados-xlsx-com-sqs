9c3bfa325e57dd3a494cca18df05cf63c6c0249b
John lennon de melo fernandes <jlfernandes91@hotmail.com>
2022-08-03 00:20:35 -0300

    adding basic impl of file reader

--- csv-database-generator/src/main/java/com/example/exceldatabasegenerator/controller/DatabaseGenerator.java
+++ csv-database-generator/src/main/java/com/example/exceldatabasegenerator/controller/DatabaseGenerator.java
@@ -1,13 +1,12 @@
 package com.example.exceldatabasegenerator.controller;
 
+import com.example.exceldatabasegenerator.DTO.LineDTO;
 import com.example.exceldatabasegenerator.services.CSVGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
+import java.util.List;
 
 @RestController
 @RequestMapping("generator")
@@ -29,5 +28,19 @@ public class DatabaseGenerator {
             return "nok";
         }
 
+    }
+
+    @GetMapping()
+    public List<LineDTO> readDatabase(
+            @RequestParam("fileName") final String fileName
+    ) {
+        try {
+            return CSVGenerator.readFile(fileName)
+                    // listando tamanho do arquivo de saída para fazer conversão para json
+                    .subList(0, 10000);
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
+        return null;
     }
 }
--- csv-database-generator/src/main/java/com/example/exceldatabasegenerator/services/CSVGenerator.java
+++ csv-database-generator/src/main/java/com/example/exceldatabasegenerator/services/CSVGenerator.java
@@ -2,14 +2,15 @@ package com.example.exceldatabasegenerator.services;
 
 import com.example.exceldatabasegenerator.DTO.LineDTO;
 import com.github.javafaker.Faker;
+import com.opencsv.CSVReaderBuilder;
 import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvValidationException;
+import com.opencsv.validators.RowValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.time.LocalDate;
@@ -81,7 +82,7 @@ public class CSVGenerator {
 
         writer.writeNext(headers);
 
-        while (inMemoryBaseIterator.hasNext()){
+        while (inMemoryBaseIterator.hasNext()) {
             final var current = inMemoryBaseIterator.next();
             writer.writeNext(current.toArray());
         }
@@ -179,5 +180,68 @@ public class CSVGenerator {
                 .setContractValue(BigDecimal.valueOf(faker.number().randomDouble(2, 100000000, 999999999)))
                 .setContractWarningLevel(contractLevels[faker.random().nextInt(0, 2)]);
     }
+
+
+    public List<LineDTO> readFile(
+            final String fileName
+    ) throws FileNotFoundException {
+
+
+        final List<LineDTO> result = new ArrayList();
+        final var url = outputPath + File.separator + fileName;
+        final var reader = new CSVReaderBuilder(new FileReader(url))
+                .withRowValidator(fabricateCustomValidator())
+                .build();
+
+        final var readerIterator = reader.iterator();
+
+        //pulando a primeira linha ( header )
+        if(readerIterator.hasNext()){
+            readerIterator.next();
+        }
+
+
+        while (readerIterator.hasNext()){
+            final var current = readerIterator.next();
+            final LineDTO lineDTO = new LineDTO();
+
+            lineDTO.setCustomerName(current[0])
+                    .setCustomerType(current[1])
+                    .setCustomerDocument(current[2])
+                    .setPhoneDDD(Integer.parseInt(current[3]))
+                    .setPhoneIsChecked(current[4].equals("1"))
+                    .setPhoneNumber(Integer.parseInt(current[5]))
+                    .setCompanyDocument(current[6])
+                    .setCompanyName(current[7])
+                    .setContractNumber(current[8])
+                    .setContractValue(new BigDecimal(current[9]))
+                    .setContractInstallmentsSize(Integer.parseInt(current[10]))
+                    .setContractReferenceDate(LocalDate.parse(current[11]))
+                    .setContractWarningLevel(current[12])
+            ;
+
+            result.add(lineDTO);
+
+        }
+
+        return result;
+    }
+
+
+    private RowValidator fabricateCustomValidator() {
+        return new RowValidator() {
+            @Override
+            public boolean isValid(String[] strings) {
+                System.out.println(strings);
+                return true;
+            }
+
+            @Override
+            public void validate(String[] strings) throws CsvValidationException {
+
+            }
+        };
+    }
+
 
 }
